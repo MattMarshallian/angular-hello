4e063822ce93db3db4d31f93bd114cb2983ab3e6
Matt Marshallian <matt.marshallian@gmail.com>
2020-06-16 19:51:22 +0200

    Added slider demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { MatSliderModule } from '@angular/material/slider';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderDemoComponent } from './mat-slider-demo/mat-slider-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatSliderDemoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mat-slider-demo/mat-slider-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-slider-demo',
+  templateUrl: './mat-slider-demo.component.html',
+  styleUrls: ['./mat-slider-demo.component.css', '../app.component.css']
+})
+export class MatSliderDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
