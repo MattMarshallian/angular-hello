04365f13544ab6826d67813e5b323a54dd177e3c
Matt Marshallian <matt.marshallian@gmail.com>
2020-06-16 20:32:59 +0200

    Added autocomplete demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderDemoComponent } from './mat-slider-demo/mat-slider-demo.component';
+import { MatAutocompleteDemoComponent } from './mat-autocomplete-demo/mat-autocomplete-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatSliderDemoComponent
+    MatSliderDemoComponent,
+    MatAutocompleteDemoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mat-autocomplete-demo/mat-autocomplete-demo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-mat-autocomplete-demo',
+  templateUrl: './mat-autocomplete-demo.component.html',
+  styleUrls: ['./mat-autocomplete-demo.component.css', '../app.component.css']
+})
+export class MatAutocompleteDemoComponent implements OnInit {
+
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three', 'Dupa'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
