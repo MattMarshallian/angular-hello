8a54c912c7061e0cf1766d81d3c820abae0e778d
Matt Marshall <matt.marshallian@gmail.com>
2020-08-22 18:43:56 +0200

    added mat-toolbar component

---
+++ src/app/mat-toolbar-demo/mat-toolbar-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-toolbar-demo',
+  templateUrl: './mat-toolbar-demo.component.html',
+  styleUrls: ['./mat-toolbar-demo.component.css']
+})
+export class MatToolbarDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
